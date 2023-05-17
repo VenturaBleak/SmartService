5bc4927d40f6a893fe8e8441f5ceab31da892126
VenturaHaze <ijumperer@gmail.com>
2023-05-17 19:18:10 +0200

    Benni's Pycharm

--- data_preparation_time_series.py
+++ data_preparation_time_series.py
@@ -2,19 +2,19 @@ import pandas as pd
 import numpy as np
 
 # Function to create sliding windows for the time series data
-def create_sliding_window(data, window_size):
+def create_sliding_window(data, observation_period, forecasting_period):
     X, y = [], []
     # Loop over the data to create sliding windows
-    for i in range(len(data) - window_size):
+    for i in range(len(data) - observation_period - forecasting_period + 1):
         # Append the windowed data to X (input features)
         # and the next data point to y (output)
         # Note that we reverse the windowed data so it's in order t, t-1, t-2, ...
-        X.append(data[i:i + window_size][::-1])
-        y.append(data[i + window_size])
+        X.append(data[i:i + observation_period][::-1])
+        y.append(data[i + observation_period:i + observation_period + forecasting_period])
     return np.array(X), np.array(y)
 
 # Function to prepare the time series data
-def prepare_time_series_data(data, window_size, location_column, time_column, target_column):
+def prepare_time_series_data(data, observation_period, forecasting_period, location_column, time_column, target_column):
     # Sort the data by Location ID and Timestamp
     data = data.sort_values([location_column, time_column])
     # Group the data by Location ID
@@ -26,7 +26,7 @@ def prepare_time_series_data(data, window_size, location_column, time_column, ta
         # Extract the target time series data
         kwh_data = group[target_column].values
         # Create sliding windows for this group
-        X_group, y_group = create_sliding_window(kwh_data, window_size)
+        X_group, y_group = create_sliding_window(kwh_data, observation_period, forecasting_period)
         # Add the windows to our lists
         X.extend(X_group)
         y.extend(y_group)
@@ -44,20 +44,21 @@ if __name__ == '__main__':
         'Avg Household Size': [6, 6, 6, 7, 6, 8, 7, 8, 8, 9]
     })\
 
-    WINDOW_SIZE = 2
     TARGET_COLUMN = 'kwh/day'
     LOCATION_COLUMN = 'Location ID'
     TIME_COLUMN = 'Timestamp'
+    OBSERVATION_PERIOD = 1
+    FORECASTING_PERIOD = 2
 
     # fetch feature names, that is all columns except the target column and the time column and the location column
     feature_names = [col for col in data.columns if col not in [TARGET_COLUMN, TIME_COLUMN, LOCATION_COLUMN]]
 
     # Prepare the data
-    X, y, grouped_data = prepare_time_series_data(data, WINDOW_SIZE, LOCATION_COLUMN, TIME_COLUMN, TARGET_COLUMN)
-
+    X, y, grouped_data = prepare_time_series_data(data, OBSERVATION_PERIOD, FORECASTING_PERIOD, LOCATION_COLUMN,
+                                                  TIME_COLUMN, TARGET_COLUMN)
 
     # Generate dynamic column names based on the window size
-    column_names = [[LOCATION_COLUMN, TIME_COLUMN, TARGET_COLUMN + '(t+1)'] + [f'{TARGET_COLUMN}(t-{i})' for i in range(WINDOW_SIZE)] + feature_names]
+    column_names = [[LOCATION_COLUMN, TIME_COLUMN] + [f'{TARGET_COLUMN}(t+{i})' for i in range(FORECASTING_PERIOD, 0, -1)] + [f'{TARGET_COLUMN}(t-{i})' for i in range(OBSERVATION_PERIOD)] + feature_names]
 
     # Initialize an empty DataFrame with the dynamic column names
     processed_data = pd.DataFrame(columns=column_names)
@@ -66,13 +67,13 @@ if __name__ == '__main__':
     # Loop over each group
     for loc_id, group in grouped_data:
         # Loop over each data point in the group
-        for i in range(group.shape[0] - WINDOW_SIZE):
+        for i in range(group.shape[0] - OBSERVATION_PERIOD - FORECASTING_PERIOD + 1):
             # Extract the timestamp for t+1
-            timestamp = group.iloc[i + WINDOW_SIZE][TIME_COLUMN]
+            timestamp = group.iloc[i + OBSERVATION_PERIOD + FORECASTING_PERIOD - 1][TIME_COLUMN]
             # Extract the other features for the last data point in the window
-            extra_features = group.iloc[i + WINDOW_SIZE - 1][feature_names].values
+            extra_features = group.iloc[i + OBSERVATION_PERIOD - 1][feature_names].values
             # Add the data to our processed data DataFrame
-            processed_data.loc[idx] = [loc_id, timestamp, y[idx]] + list(X[idx]) + list(extra_features)
+            processed_data.loc[idx] = [loc_id, timestamp] + list(y[idx][::-1]) + list(X[idx]) + list(extra_features)
             idx += 1
 
     # Print the processed data
